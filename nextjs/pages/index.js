8f4d0ee18a118a879a9996c263aeaeb0b5f4dfa5
pattaraphum1st <66110039@kmitl.ac.th>
2024-10-01 00:04:15 +0700

    Update index.js

--- nextjs/pages/index.js
+++ nextjs/pages/index.js
@@ -1,19 +1,49 @@
-import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import Grid from "@mui/material/Grid2";
-import { Box, Typography, Button, TextField } from "@mui/material"; // Use by LoginForm
-import useBearStore from "@/store/useBearStore";
+// pages/index.js
 
-function Home() {
+import React from "react";
+import Link from "next/link";
+import { Box, Typography, Button, Grid } from "@mui/material";
+
+export default function Home() {
   return (
-    <>
+    <Box sx={{ backgroundColor: "#355364", minHeight: "100vh", padding: "20px" }}>
+      {/* Header */}
+      <Typography variant="h3" sx={{ color: "#fff", textAlign: "center", marginBottom: "40px" }}>
+        Slave Card Game Dashboard
+      </Typography>
 
-      <main>
-        <Box>This is index page</Box>
-      </main>
-    </>
+      {/* Navigation Grid */}
+      <Grid container spacing={3} justifyContent="center">
+        {/* Links to each player page */}
+        <Grid item>
+          <Link href="/player1" passHref>
+            <Button variant="contained" sx={{ backgroundColor: "#ff5e15", minWidth: "200px" }}>
+              Go to Player 1 Page
+            </Button>
+          </Link>
+        </Grid>
+        <Grid item>
+          <Link href="/player2" passHref>
+            <Button variant="contained" sx={{ backgroundColor: "#ff5e15", minWidth: "200px" }}>
+              Go to Player 2 Page
+            </Button>
+          </Link>
+        </Grid>
+        <Grid item>
+          <Link href="/player3" passHref>
+            <Button variant="contained" sx={{ backgroundColor: "#ff5e15", minWidth: "200px" }}>
+              Go to Player 3 Page
+            </Button>
+          </Link>
+        </Grid>
+        <Grid item>
+          <Link href="/player4" passHref>
+            <Button variant="contained" sx={{ backgroundColor: "#ff5e15", minWidth: "200px" }}>
+              Go to Player 4 Page
+            </Button>
+          </Link>
+        </Grid>
+      </Grid>
+    </Box>
   );
 }
-
-export default Home;
