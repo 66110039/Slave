8ebc88902c5b542ffe1c244f75eb439a8500b84e
pattaraphum1st <66110039@kmitl.ac.th>
2024-10-04 22:09:12 +0700

    Update app.py

--- fastapi/app.py
+++ fastapi/app.py
@@ -3,11 +3,22 @@ from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from database import *
 from routes.users import router
-
+from routes.leaderboard import router as leaderboard_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # Allow requests from frontend
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(router, prefix="/api")
+app.include_router(leaderboard_router, prefix="/api")
 
 @app.on_event("startup")
 async def startup():
@@ -16,3 +27,4 @@ async def startup():
 @app.on_event("shutdown")
 async def shutdown():
     await disconnect_db()
+
